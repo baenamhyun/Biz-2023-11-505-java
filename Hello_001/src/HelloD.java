0048e2977008606261e64390a458e0dd3a8ddc10
baenamhyun <nh1336@naver.com>
2023-11-21 16:23:13 +0900

    자바

---
+++ Hello_001/src/HelloD.java
@@ -0,0 +1,11 @@
+
+public class HelloD {
+	public static void main(String[] args) {
+		System.out.println(30.0 + 40.0);
+		System.out.println(30.0 - 40.0);
+		System.out.println(30.0 * 40.0);
+		System.out.println(30.0 / 40.0);
+		System.out.println(30.0 % 40.0);
+	}
+
+}
